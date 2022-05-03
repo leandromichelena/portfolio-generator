5b07290e3cd3d843b350f9e7366d6d7674ecbf32
Leandro Michelena <leandromichelena@gmail.com>
2022-05-02 22:29:58 -0400

    Created HTML structure from input

--- app.js
+++ app.js
@@ -1,15 +1,61 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
+const mockData = {
+    name: 'Leandro Michelena',
+    github: 'leandromichelena',
+    confirmAbout: false,
+    // about:
+        // 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+    projects: [
+        {
+            name: 'Run Buddy',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['HTML', 'CSS'],
+            link: 'https://github.com/leandromichelena/run-buddy',
+            feature: true,
+            confirmAddProject: true
+        },
+        {
+            name: 'Taskinator',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['JavaScript', 'HTML', 'CSS'],
+            link: 'https://github.com/leandromichelena/taskinator',
+            feature: true,
+            confirmAddProject: true
+        },
+        {
+            name: 'Taskmaster Pro',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+            languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+            link: 'https://github.com/leandromichelena/taskmaster-pro',
+            feature: false,
+            confirmAddProject: true
+        },
+        {
+            name: 'Robot Gladiators',
+            description:
+                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+            languages: ['JavaScript'],
+            link: 'https://github.com/leandromichelena/robot-gladiators',
+            feature: false,
+            confirmAddProject: false
+        }
+    ]
+};
 
-// fs.writeFile('index.html', generatePage(pageHTML, github), err => {
-//     if (err) throw err;
+const pageHTML = generatePage(mockData);
 
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+fs.writeFile('index.html', generatePage(mockData), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -109,7 +155,7 @@ Add a New Project
                     return true;
                 }
                 else {
-                    console.log('Please enter your project description!');
+                    console.log('Please enter your project Github link!');
                     return false;
                 }
             }
@@ -137,8 +183,15 @@ Add a New Project
     });
 };
 
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
+// promptUser()
+//     .then(promptProject)
+//     .then(portfolioData => {
+        //const pageHTML = 
+        generatePage(mockData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+
+          console.log('Page created! Check out index.html in this directory to see it!');
+        });
+    // });
